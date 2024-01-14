1344046233570fbab7700c2db284c3f724902eed
Julio Zittei <julio.zittei96@gmail.com>
2024-01-14 20:55:01 -0300

    feat: add campaignService

---
+++ internal/domain/service/campaignService.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"github.com/JulioZittei/go-job-mail-service/internal/domain/contract"
+	internalerrors "github.com/JulioZittei/go-job-mail-service/internal/domain/internalErrors"
+	"github.com/JulioZittei/go-job-mail-service/internal/domain/model"
+	"github.com/JulioZittei/go-job-mail-service/internal/domain/repository"
+)
+
+type CampaignService struct {
+	Repository repository.Repository
+}
+
+func (s *CampaignService) Create(campaignInput *contract.NewCampaignInput) (id string, err error) {
+	campaign, err := model.NewCampaign(campaignInput.Name, campaignInput.Content, campaignInput.Emails)
+	if err != nil {
+		return "", err
+	}
+
+	err = s.Repository.Save(campaign)
+	if err != nil {
+		return "", internalerrors.ErrInternal
+	}
+	
+	return campaign.ID, err
+}
