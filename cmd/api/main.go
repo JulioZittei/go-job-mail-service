eff02f8efb70bafa6bf177fac3d6a636bd6d8b9c
Julio Zittei <julio.zittei96@gmail.com>
2024-01-16 18:17:07 -0300

    feat: add api doc and create server application

---
+++ cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/JulioZittei/go-job-mail-service/internal/api/v1/controller"
+	exceptionhandler "github.com/JulioZittei/go-job-mail-service/internal/api/v1/exceptionHandler"
+	"github.com/JulioZittei/go-job-mail-service/internal/domain/service"
+	"github.com/JulioZittei/go-job-mail-service/internal/infrastructure/database"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	service := service.CampaignService{
+		Repository: &database.CampaignRepository{},
+	}
+
+	controller := &controller.CampaignController{
+		CampaignService: service,
+	}
+	
+	r.Post("/campaign", exceptionhandler.ExceptionHandler(controller.CampaignPost))
+	r.Get("/campaign", exceptionhandler.ExceptionHandler(controller.CampaignGet))
+
+	http.ListenAndServe(":3000", r)
+}
