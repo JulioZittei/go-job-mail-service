fd4d2ab60bcdd3fecde5e27eb799c79f568d6954
Julio Zittei <julio.zittei96@gmail.com>
2024-01-23 22:17:59 -0300

    feat: update repository interface

--- internal/domain/repository/repository.go
+++ internal/domain/repository/repository.go
@@ -4,6 +4,8 @@ import "github.com/JulioZittei/go-job-mail-service/internal/domain/model"
 
 type Repository interface {
 	Save(campaign *model.Campaign) error
+	Update(campaign *model.Campaign) error
 	Get() ([]model.Campaign, error)
 	GetById(id string) (*model.Campaign, error)
+	Delete(campaign *model.Campaign) error
 }
