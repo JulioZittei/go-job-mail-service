746e1be23a73da92232636e942cd8c3e08a03da7
Julio Zittei <julio.zittei96@gmail.com>
2024-01-23 22:20:20 -0300

    feat: update campaign model

--- internal/domain/model/campaign.go
+++ internal/domain/model/campaign.go
@@ -8,22 +8,34 @@ import (
 )
 
 type Contact struct {
-	Email string `json:"email" validate:"email"`
+	ID         string `gorm:"primaryKey,size:50"`
+	Email      string `gorm:"size:100" validate:"email"`
+	CampaignId string `gorm:"size:50"`
 }
 
 const (
-	Pending string = "PENDING"
-	Started string = "STARTED"
-	Done string = "DONE"
+	Pending  string = "PENDING"
+	Canceled string = "CANCELED"
+	Deleted  string = "DELETED"
+	Started  string = "STARTED"
+	Done     string = "DONE"
 )
 
 type Campaign struct {
-	ID string `json:"id" validate:"required"`
-	Name string `json:"name" validate:"min=5,max=24"`
-	Content string `json:"content" validate:"min=5,max=1024"`
-	Contacts []*Contact `json:"contacts" validate:"min=1,dive"`
-	Status string `json:"status"`
-	CreatedAt time.Time `json:"createdAt" validate:"required"`
+	ID        string     `gorm:"primaryKey,size:50" validate:"required"`
+	Name      string     `gorm:"size:100" validate:"min=5,max=24"`
+	Content   string     `gorm:"size:1024" validate:"min=5,max=1024"`
+	Contacts  []*Contact `validate:"min=1,dive"`
+	Status    string     `gorm:"size:20"`
+	CreatedAt time.Time  `validate:"required"`
+}
+
+func (c *Campaign) Cancel() {
+	c.Status = Canceled
+}
+
+func (c *Campaign) Delete() {
+	c.Status = Deleted
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
@@ -31,17 +43,18 @@ func NewCampaign(name string, content string, emails []string) (*Campaign, error
 	contacts := make([]*Contact, len(emails))
 	for i, value := range emails {
 		contacts[i] = &Contact{
+			ID:    xid.New().String(),
 			Email: value,
 		}
 	}
-	
+
 	campaign := &Campaign{
-		ID: xid.New().String(),
-		Name: name,
-		Content: content,
+		ID:        xid.New().String(),
+		Name:      name,
+		Content:   content,
 		CreatedAt: time.Now(),
-		Contacts: contacts,
-		Status: Pending,
+		Contacts:  contacts,
+		Status:    Pending,
 	}
 
 	err := validator.ValidateStruct(campaign)
