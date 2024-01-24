40fb454edd7919443767e6bc75df5a61295fe781
Julio Zittei <julio.zittei96@gmail.com>
2024-01-23 22:11:06 -0300

    feat: add delete service and campaing not found error handling

--- internal/domain/service/campaignService.go
+++ internal/domain/service/campaignService.go
@@ -1,15 +1,19 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/JulioZittei/go-job-mail-service/internal/domain/contract"
 	internalerrors "github.com/JulioZittei/go-job-mail-service/internal/domain/internalErrors"
 	"github.com/JulioZittei/go-job-mail-service/internal/domain/model"
 	"github.com/JulioZittei/go-job-mail-service/internal/domain/repository"
+	"gorm.io/gorm"
 )
 
 type CampaignService interface {
 	Create(campaignInput *contract.NewCampaignInput) (id string, err error)
 	GetById(id string) (*contract.CampaignOutput, error)
+	Delete(id string) error
 }
 
 type CampaignServiceImpl struct {
@@ -26,19 +30,45 @@ func (s *CampaignServiceImpl) Create(campaignInput *contract.NewCampaignInput) (
 	if err != nil {
 		return "", internalerrors.NewErrInternal()
 	}
-	
+
 	return campaign.ID, err
 }
 
 func (s *CampaignServiceImpl) GetById(id string) (*contract.CampaignOutput, error) {
 	campaign, err := s.Repository.GetById(id)
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, internalerrors.NewErrCampaignNotFound()
+		}
 		return nil, internalerrors.NewErrInternal()
 	}
 	return &contract.CampaignOutput{
-		ID: campaign.ID,
-		Name: campaign.Name,
-		Content: campaign.Content,
-		Status: campaign.Status,
+		ID:           campaign.ID,
+		Name:         campaign.Name,
+		Content:      campaign.Content,
+		Status:       campaign.Status,
+		EmailsToSend: len(campaign.Contacts),
 	}, nil
+}
+
+func (s *CampaignServiceImpl) Delete(id string) error {
+	campaign, err := s.Repository.GetById(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return internalerrors.NewErrCampaignNotFound()
+		}
+		return internalerrors.NewErrInternal()
+	}
+
+	if campaign.Status != model.Pending {
+		return errors.New("campaign could not be deleted, because is not pending")
+	}
+
+	campaign.Delete()
+	err = s.Repository.Delete(campaign)
+	if err != nil {
+		return internalerrors.NewErrInternal()
+	}
+
+	return nil
 }
