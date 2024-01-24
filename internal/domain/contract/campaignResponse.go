077908716ed6fd22b7ed4dc431664f3efb4566c5
Julio Zittei <julio.zittei96@gmail.com>
2024-01-23 22:19:20 -0300

    feat: add campaign response

--- internal/domain/contract/campaignResponse.go
+++ internal/domain/contract/campaignResponse.go
@@ -1,8 +1,9 @@
 package contract
 
 type CampaignOutput struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Content string `json:"content"`
-	Status string `json:"status"`
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Content      string `json:"content"`
+	Status       string `json:"status"`
+	EmailsToSend int    `json:"emailsToSend"`
 }
