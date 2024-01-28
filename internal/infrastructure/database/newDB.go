07d5308bc53a0f579ed2c0d8fb9f2daaffc458f6
Julio Zittei <julio.zittei96@gmail.com>
2024-01-28 14:58:33 -0300

    chore: add database info into .env file

--- internal/infrastructure/database/newDB.go
+++ internal/infrastructure/database/newDB.go
@@ -1,13 +1,15 @@
 package database
 
 import (
+	"os"
+
 	"github.com/JulioZittei/go-job-mail-service/internal/domain/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDB() *gorm.DB {
-	dsn := "host=localhost user=mail_service_job_user password=mail_service_job_pass dbname=mail_job_service port=5432 sslmode=disable"
+	dsn := os.Getenv("DATABASE")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("fail to connect to database")
